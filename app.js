c36a49eb508484ab571d8e92c4cc864de4994254
Jaime López <jaime.lopez.lopez@live.com>
2016-06-27 10:57:24 +0200

    + Added middleware function with error catching

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.listen(port, function(req, res, next) {
    //console.log('Listening on port ' + port + '...');
 });
 
+app.use(function(req, res, next) {
+    try {
+        next();
+    } catch (err) {
+        
+    }
+});
+
 app.get('/', function(req, res, next) {
     res.send('Hello from the root');
 });
