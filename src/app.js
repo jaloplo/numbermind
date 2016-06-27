5da008d0014ebe475c7bc41af3737eea1bfed39d
Jaime López <jaime.lopez.lopez@live.com>
2016-06-27 11:05:59 +0200

    * Modified the app.js script in src folder copying the same code as the app.js in root folder

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 var express = require('express');
 
-var port = process.env.PORT || 8080;
-
-
+var port = process.env.PORT || 1337;
 var app = new express();
+
 app.listen(port, function(req, res, next) {
-    console.log('Listening on port ' + port + '...');
+   //console.log('Listening on port ' + port + '...');
 });
 
 app.use(function(req, res, next) {
-    console.log('Request for ' + req.originalUrl + '...');
-    next();
+    try {
+        next();
+    } catch (err) {
+
+    }
+});
+
+app.get('/', function(req, res, next) {
+    res.send('Hello from the source folder');
 });
 
 app.use(function(req, res, next) {
-    res.status(404).send('Page not found');
+    res.status(404).send('Page not found from express and source folder');
 });
