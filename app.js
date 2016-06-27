c144f555df5b83034be8e7102dde10a6c787868e
Jaime López <jaime.lopez.lopez@live.com>
2016-06-27 10:45:52 +0200

    + Added some methods on app.js

--- app.js
+++ app.js
@@ -3,20 +3,19 @@ var express = require('express');
 var port = process.env.PORT || 1337;
 var app = new express();
 
+app.listen(port, function(req, res, next) {
+    console.log('Listening on port ' + port + '...');
+});
+
+app.use(function(req, res, next) {
+    console.log('Request for ' + req.originalUrl + '...');
+    next();
+});
+
+app.use(function(req, res, next) {
+    res.status(404).send('Page not found');
+});
+
 app.get('/', function(req, res, next) {
     res.send('Hello from the root');
 });
-
-app.listen(port, function(req, res, next) {
-    //console.log('Server listening on port ' + port);
-    //console.log('Running...');
-});
-
-/*
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
-*/
