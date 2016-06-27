449776675e3ae085f66077a15c708e3378c1c875
Jaime López <jaime.lopez.lopez@live.com>
2016-06-27 13:55:11 +0200

    + Added nunjucks as a module for render views * Modified home page route to render numbermind view

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 var express = require('express');
+var nunjucks = require('nunjucks');
 
 var port = process.env.PORT || 1337;
 var app = new express();
+
+
+var engine = nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    watch: true
+});
+
+app.set('views', './views');
+
+app.use(function(req, res, next) {
+    console.log('[' + req.method + ']::' + req.originalUrl);
+    next();
+});
+
 
 app.listen(port, function(req, res, next) {
    console.log('Listening on port ' + port + '...');
 });
 
-app.use(function(req, res, next) {
-    next();
-});
-
 app.get('/', function(req, res, next) {
-    res.send('Hello from the source folder');
+    res.render('numbermind.html');
 });
 
 app.use(function(req, res, next) {
