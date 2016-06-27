718c8dd1076cd1f6b180654ed2d3e5b9ece12f90
Jaime López <jaime.lopez.lopez@live.com>
2016-06-27 10:26:11 +0200

    * Modified app.js on the roof folder to accomplish a simple code and try to show on azure.

--- app.js
+++ app.js
@@ -1,26 +1,6 @@
-var express = require('express');
-
-var port = process.env.PORT || 8080;
-
-
-var app = new express();
-app.listen(port, function(req, res, next) {
-    console.log('Listening on port ' + port + '...');
-});
-
-app.use(function(req, res, next) {
-    console.log('Request for ' + req.originalUrl + '...');
-    next();
-});
-
-app.get('/', function(req, res, next) {
-    return 'Page from node.js app in azure.';
-});
-
-app.get('/hello', function(req, res, next) {
-    return 'BIG HELLO from node.js app in azure.';
-});
-
-app.use(function(req, res, next) {
-    res.status(404).send('Page not found');
-});
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
