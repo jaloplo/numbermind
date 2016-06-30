88c53524e8f230667f78c8f6f7ada42d3f222c51
Jaime López <jaime.lopez.lopez@live.com>
2016-06-30 13:17:13 +0200

    * Modifications on engine, mvc and html

--- src/static/js/mvc.js
+++ src/static/js/mvc.js
@@ -13,6 +13,7 @@ Mivu.Events.on(document, 'readystatechange', function() {
         var game = null;
 
         var gameModel = new Mivu.Mvc.Model({
+            firstGame: true,
             status: GAME_STATUS.CREATING,
             attempts: 0,
             number: null,
@@ -25,37 +26,48 @@ Mivu.Events.on(document, 'readystatechange', function() {
             actions: {
                 onRender: function() {
                     var status = this.model.get('status');
+                    var firstGame = this.model.get('firstGame');
+
+                    if(firstGame) {
+                        var styles = this.view.get()[0].className;
+                        if(styles.indexOf('show-all') !== -1) {
+                            this.view.get()[0].className = styles.replace('show-all', 'hide-all');
+                        } else if(styles.indexOf('hide-all') === -1){
+                            this.view.get()[0].className = styles + ' hide-all';
+                        }
+                    } else {
+                        this.view.get()[0].className = this.view.get()[0].className.replace('hide-all', 'show-all');
+                    }
+                    
 
                     switch(status) {
                         case GAME_STATUS.CREATING:
-                            console.log('>>> CREATING');
                             game = new Game();
                             this.model.set('status', GAME_STATUS.PLAYING);
                             break;
 
                         case GAME_STATUS.PLAYING:
-                            console.log('>>> PLAYING');
                             var attempts = this.model.get('attempts');
                             var hits = this.model.get('hits');
 
                             if(hits === 4) {
                                 this.model.set('status', GAME_STATUS.WIN);
+                                this.model.set('firstGame', false);
                                 break;
                             }
 
-                            if(attempts === 10) {
+                            if(attempts === 9) {
                                 this.model.set('status', GAME_STATUS.LOSE);
+                                this.model.set('firstGame', false);
                                 break;
                             }
 
                             break;
 
                         case GAME_STATUS.LOSE:
-                            console.log('>>> LOSE');
                             break;
 
                         case GAME_STATUS.WIN:
-                            console.log('>>> WIN');
                             break;
                     }
                 },
@@ -73,6 +85,27 @@ Mivu.Events.on(document, 'readystatechange', function() {
         });
 
         newGameView.render();
+
+        
+
+        var messageView = new Mivu.Mvc.View({
+            elem: document.getElementsByClassName('message-group')[0],
+            model: gameModel,
+            actions: {
+                onRender: function() {
+                    var status = this.model.get('status');
+                    if(status === GAME_STATUS.WIN) {
+                        this.view.get('message-text')[0].innerHTML = '¡¡¡Has ganado el juego!!!';
+                    } else if(status === GAME_STATUS.LOSE) {
+                        this.view.get('message-text')[0].innerHTML = 'Has perdido. Inténtalo otra vez.';
+                    } else {
+                        this.view.get('message-text')[0].innerHTML = '';
+                    }
+                },
+            },
+        });
+
+        messageView.render();
         
 
         var resultsTableModel = new Mivu.Mvc.ModelSet();
@@ -94,9 +127,12 @@ Mivu.Events.on(document, 'readystatechange', function() {
                     input.disabled = status === GAME_STATUS.PLAYING ? '' : 'true';
 
                     tip.innerHTML = error;                    
-                    control.className = control.className.replace('error', '');                    
+                    control.className = control.className.replace('error', '');
+                    button.className = button.className.replace('red', 'blue');
+
                     if(undefined !== error && null != error && '' !== error) {
-                        control.className += ' error'; 
+                        control.className += ' error';
+                        button.className = button.className.replace('blue', 'red'); 
                     }
                 },
 
@@ -162,20 +198,24 @@ Mivu.Events.on(document, 'readystatechange', function() {
             elem: document.getElementsByClassName('resultstable-list')[0],
             model: resultsTableModel,
             view: {
-                tag: 'li',
+                tag: 'tr',
                 template: '\
-                    <div class="all-20 resultstable-attemp"></div> \
-                    <div class="all-40 resultstable-number"></div> \
-                    <div class="all-40 resultstable-result"></div>',
+                    <td class="all-20 resultstable-attemp"></td> \
+                    <td class="all-20 resultstable-number"></td> \
+                    <td class="all-60 resultstable-result"></td>',
                 actions: {
                     onRender: function() {
-                        var attemp = this.model.get('attempt') + 1;
+                        var attempt = this.model.get('attempt') + 1;
                         var number = this.model.get('value');
                         var result = this.model.get('result') || 'There is no result yet';
                         
-                        this.view.get('resultstable-attemp')[0].innerHTML = attemp;
+                        this.view.get('resultstable-attemp')[0].innerHTML = attempt;
                         this.view.get('resultstable-number')[0].innerHTML = number;
-                        this.view.get('resultstable-result')[0].innerHTML = 'Hits: ' + result.hits + ' Almost hits: ' + result.almost_hits;
+                        this.view.get('resultstable-result')[0].innerHTML = 'acertados: ' + result.hits + ' - en otra posición: ' + result.almost_hits;
+
+                        if(result.hits === 4) {
+                            this.view.get()[0].className += 'orange';
+                        }
                     },
                 },
             },
@@ -183,7 +223,7 @@ Mivu.Events.on(document, 'readystatechange', function() {
 
         resultsTableView.render();
 
-
+/*
         var loggerView = new Mivu.Mvc.ViewSet({
             root: document.getElementsByClassName('logger-group')[0],
             elem: document.getElementsByClassName('logger-list')[0],
@@ -204,5 +244,6 @@ Mivu.Events.on(document, 'readystatechange', function() {
         });
 
         loggerView.render();
+        */
     }
 });
